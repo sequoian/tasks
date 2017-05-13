16c85de9aee45ae483dfd1ca939ab6fb435c8a24
Zach Askins <zach303@msn.com>
2017-05-13 13:13:30 -0700

    basic scaffolding for task views

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react';
+import './App.css';
+
+const Pages = {
+  SCHEDULED: 'scheduled',
+  OVERDUE: 'overdue',
+  ACTIVE: 'active',
+  COMPLETE: 'complete',
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: Pages.SCHEDULED
+    }
+    this.changePage = this.changePage.bind(this);
+  }
+  changePage(page) {
+    this.setState({currentPage: page});
+  }
   render() {
     return (
       <div className="App">
-        <h2>tasks</h2>
+        <Tasks header={this.state.currentPage} />
+        <Nav action={this.changePage} />
       </div>
     );
   }
 }
+
+class Nav extends Component {
+  render() {
+    return (
+      <ul className="nav">
+        <li>
+          <button type="button" onClick={() => this.props.action(Pages.SCHEDULED)}>
+            Scheduled
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={() => this.props.action(Pages.OVERDUE)}>
+            Overdue
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={() => this.props.action(Pages.ACTIVE)}>
+            Active
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={() => this.props.action(Pages.COMPLETE)}>
+            Completed
+          </button>
+        </li>
+      </ul>
+    );
+  }
+}
+
+class Tasks extends Component {
+  render() {
+    return (
+      <div className="tasks">
+        <h2>{this.props.header}</h2>
+        {this.props.comp}
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
