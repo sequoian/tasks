f937099d4ede3ccdac9825ee355a93510216f221
Zach Askins <zach303@msn.com>
2017-10-28 13:05:24 -0700

    change mode with button press

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import FilterNav from './FilterNav'
 import AddTask from '../containers/AddTask'
 import VisibleTaskList from '../containers/VisibleTaskList'
+import EditButton from '../containers/EditButton'
 
 const App = () => (
   <div id="app">
+    <EditButton />
     <AddTask />
     <FilterNav />
     <VisibleTaskList />
